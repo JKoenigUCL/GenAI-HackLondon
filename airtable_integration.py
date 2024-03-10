2b25313b57b4040a8eae95c2a64872676810f4c4
ngenethlis <ngenethlis@gmail.com>
2024-03-10 13:14:20 +0000

    airtable integration done

--- airtable_integration.py
+++ airtable_integration.py
@@ -5,53 +5,36 @@ import json
 import requests
 
 
-class AirTable:
+class AirTableManager:
     def __init__(self):
         self.secrets = SecretManager()
 
-    def saveArticle(self, article_json):
-        self.airtable.create(article_json)
+    def saveArticles(self, articleList: list):
+        dataJson = articleList
 
-    def saveArticles(self):
-        with open('final_table_format.json') as f:
-            dataJson = json.load(f)
-            # Replace with your actual API key
-            api_key = self.secrets.airtable_personal_access_token
+        url = f"https://api.airtable.com/v0/app{self.secrets.airtable_base_id}/tbl{self.secrets.airtable_id}"
 
-            url = f"https://api.airtable.com/v0/app{self.secrets.airtable_base_id}/tbl{self.secrets.airtable_id}"
+        headers = {
+            "Authorization": f"Bearer {self.secrets.airtable_personal_access_token}",
+            "Content-Type": "application/json"
+        }
 
-            headers = {
-                "Authorization": f"Bearer {api_key}",
-                "Content-Type": "application/json"
-            }
-
-            data = {"records": []}
-            for article in dataJson:
-                temp = {
-                    "fields": {
-                        "Title": article["title"],
-                        "Description": article["description"],
-                        "Article Plan": article["article_plan"]
-                    }
+        data = {"records": []}
+        for article in dataJson:
+            temp = {
+                "fields": {
+                    "Name": article["title"],
+                    "Description": article["description"],
+                    "Article Plan": article["article_plan"]
                 }
-                data["records"].append(temp)
+            }
+            data["records"].append(temp)
 
-            print(data)
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        # check if the request was successful
+        if response.status_code == 200:
+            print("Success")
+        else:
+            print(f"Failed to save articles to Airtable. \
+            Status code: {response.status_code}, Response: {response.text}")
 
-            response = requests.post(url, headers=headers, data=data)
-
-            # Check for successful response (optional)
-            if response.status_code == 200:
-                print("Article added successfully!")
-            else:
-                print(f"Error adding article: {response.text}")
-
-            print(url)
-            response = requests.post(url)
-            print(response)
-
-
-
-if __name__ == "__main__":
-    airtable = AirTable()
-    airtable.saveArticles()
